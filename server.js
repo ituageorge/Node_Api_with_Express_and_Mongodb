704c8222dd3b120deac174e846da13a1a74c4448
ituageorge <ituaosagie100@gmail.com>
2020-02-14 15:16:19 +0100

    Introduction to middleware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan')
 
 //Route files
 const bootcamps = require("./routes/bootcamps")
@@ -10,8 +11,15 @@ dotenv.config({ path: './config/config.env'});
 const app = express();
 
 
+// dev logging middleware
+if( process.env.NODE_ENV === "development") {
+    app.use(morgan("dev"));
+}
+
+ 
+
 //Mount routers
-app.use("/api/v177/bootcamps", bootcamps);
+app.use("/api/v1/bootcamps", bootcamps);
 
 
 const PORT = process.env.PORT || 5000;
