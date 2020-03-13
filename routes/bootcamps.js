7a3275e1c5d0415ada6d440d6ea955e8b9b2f157
ituageorge <ituaosagie100@gmail.com>
2020-03-13 19:40:10 +0100

    Advanced Result Middleware

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -11,6 +11,9 @@ const router = express.Router();
           bootcampPhotoUpload
         } = require('../controllers/bootcamps')
 
+    const Bootcamp = require('../models/Bootcamp')
+    const advancedResults = require('../middleware/advancedResults')
+
         // Include other resource routers 
         const courseRouter = require('./courses')
 
@@ -19,7 +22,7 @@ const router = express.Router();
 
     router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 
-    router.route('/').get(getBootcamps).post(createBootcamp);
+    router.route('/').get(advancedResults(Bootcamp, 'courses'), getBootcamps).post(createBootcamp);
     router.route("/:id").get(getBootcamp).put(updateBootcamp).delete(deleteBootcamp)
 
     router.route('/:id/photo').put(bootcampPhotoUpload);
