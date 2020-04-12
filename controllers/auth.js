e7542996674eb510b1ea49d452a7428f8e93134c
ituageorge <ituaosagie100@gmail.com>
2020-04-12 11:34:24 +0100

    forgot password - generate token

--- controllers/auth.js
+++ controllers/auth.js
@@ -50,28 +50,7 @@ exports.login = asyncHandler(async (req, res, next) => {
   sendTokenResponse(user, 200, res)
 });
 
-// Get token from model, create cookie and send response
-const sendTokenResponse = (user, statusCode, res) => {
-    //Create token
-   const token = user.getSignedJwtToken(); 
 
-   const options = {
-     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
-     httpOnly: true
-   };
-
-    if(process.env.NODE_ENV === 'production') {
-      options.secure = true;
-    }
-
-   res
-    .status(statusCode)
-    .cookie('token', token, options)
-    .json({
-     success: true,
-     token
-   })
-}
 
 //@desc Get current logged in user
 //@route POST /api/v1/auth/me
@@ -85,3 +64,55 @@ exports.getMe = asyncHandler(async (req, res, next) => {
       data: user 
     })
 })
+
+//@desc Forgot password
+//@route POST /api/v1/auth/mforgotpassword
+//@access Public
+
+exports.forgotPassword = asyncHandler(async (req, res, next) => {
+  const user = await User.findOne({ email: req.body.email});
+
+  if(!user) {
+    return next(new ErrorResponse('There is no user with that email', 404));
+  }
+
+  //Get reset token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save({ validateBeforeSave: false})
+  res.status(200).json({
+    success: true,
+    data: user
+  })
+
+    res.status(200).json({
+      success: true,
+      data: user 
+    })
+})
+
+
+
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = (user, statusCode, res) => {
+  //Create token
+ const token = user.getSignedJwtToken(); 
+
+ const options = {
+   expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000),
+   httpOnly: true
+ };
+
+  if(process.env.NODE_ENV === 'production') {
+    options.secure = true;
+  }
+
+ res
+  .status(statusCode)
+  .cookie('token', token, options)
+  .json({
+   success: true,
+   token
+ })
+}
