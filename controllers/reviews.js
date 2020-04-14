19e5c9ab51f587bbc0285f2199d0aa28ce7604ab
ituageorge <ituaosagie100@gmail.com>
2020-04-15 00:16:52 +0100

    Add review for bootcamp

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -21,7 +21,7 @@ exports.getReviews = asyncHandler(async (req, res, next) => {
 }
 });
 
-//description   Get a single reviews
+//description   Get a single review
 //@route        Get /api/v1/reviews/:id
 //@access       Public
 exports.getReview = asyncHandler(async (req, res, next) => {
@@ -31,11 +31,37 @@ exports.getReview = asyncHandler(async (req, res, next) => {
    });
 
    if(!review) {
-       return next(new ErrorResponse(`No review found with the id of ${req.params.id}`), 404);
+       return next(
+           new ErrorResponse(`No review found with the id of ${req.params.id}`, 404)
+       );
    }
 
    res.status(200).json({
        success: true,
        data: review
-   })
+   });
 });
+
+//description   Add review
+//@route        Post /api/v1/bootcamps/:bootcampId/reviews
+//@access       Private
+exports.addReview = asyncHandler(async (req, res, next) => {
+    req.body.bootcamp = req.params.bootcampId;
+    req.body.user = req.user.id;
+
+    const bootcamp = await Bootcamp.findById(req.params.bootcampId);
+
+    if(!bootcamp) {
+        return next(new ErrorResponse(
+        `No bootcamp with the id of ${req.params.bootcampId}`, 404
+        )
+        );
+    }
+
+    const review = await Review.create(req.body)
+ 
+    res.status(201).json({
+        success: true,
+        data: review
+    })
+ });
