02ae4a8a3efc1ccb88b747f6ab211118363abc4a
ituageorge <ituaosagie100@gmail.com>
2020-04-15 01:08:48 +0100

    Update Delete Reviews

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -65,3 +65,68 @@ exports.addReview = asyncHandler(async (req, res, next) => {
         data: review
     })
  });
+
+ //description   Update review
+//@route        Put /api/v1/reviews/:id
+//@access       Private
+exports.updateReview = asyncHandler(async (req, res, next) => {
+    let review = await Review.findById(req.params.id);
+
+    if(!review) {
+        return next(
+        new ErrorResponse(
+        `No review with the id of ${req.params.id}`, 404
+        )
+        );
+    }
+
+    //Make sure review belongs to user or user is admin
+    if(review.user.toString() !== req.user.id && req.user.role !== 'admin'){
+        return next(
+            new ErrorResponse(
+            `Not authorized to update review `, 401
+            )
+            );
+    }
+
+     review = await Review.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+ 
+    res.status(200).json({
+        success: true,
+        data: review
+    })
+ });
+
+ //description   Delet review
+//@route        delete /api/v1/reviews/:id
+//@access       Private
+exports.deleteReview = asyncHandler(async (req, res, next) => {
+    let review = await Review.findById(req.params.id);
+
+    if(!review) {
+        return next(
+        new ErrorResponse(
+        `No review with the id of ${req.params.id}`, 404
+        )
+        );
+    }
+
+    //Make sure review belongs to user or user is admin
+    if(review.user.toString() !== req.user.id && req.user.role !== 'admin'){
+        return next(
+            new ErrorResponse(
+            `Not authorized to update review `, 401
+            )
+            );
+    }
+
+    await review.remove()
+ 
+    res.status(200).json({
+        success: true,
+        data: {}
+    })
+ });
